f2a43499cfaede1476d5dd83fb974712c7f3c2a6
Soikot Shahriar <soikot.shahriaar@gmail.com>
2024-08-01 13:23:20 +0600

    first commit by ss

---
+++ PracticeJava/A_Hello.java
@@ -0,0 +1,29 @@
+// package com.company;
+
+public class A_Hello{	
+	
+	public static void main(String[]args){
+		System.out.println("Hello World");
+	}
+}
+
+/*
+package com.company :
+	Packages are used to group the related classes.
+	The "Package" keyword is used to create packages in Java.
+	Here, com.company is the name of our package.
+
+public class Hello :
+	In Java, every program must contain a class.
+	The filename and name of the class should be the same.
+	Here, we've created a class named "Hello".
+	It is the entry point to the application.
+
+public static void main(String[]args){..} :
+	This is the main() method of our Java program.
+	Every Java program must contain the main() method.
+
+System.out.println("Hello World"):
+	The above code is used to display the output on the screen.
+	Anything passed inside the inverted commas is printed on the screen as plain text.
+*/
