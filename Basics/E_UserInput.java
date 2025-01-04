527d903b4968a9508a9944535ea87ce7b57b31cb
Soikot Shahriar <soikot.shahriaar@gmail.com>
2025-01-04 22:10:36 +0600

    Updated

--- Basics/E_UserInput.java
+++ Basics/E_UserInput.java
@@ -9,21 +9,20 @@ import java.util.Scanner; // Import the Scanner class
 public class E_UserInput {
 
     public static void main(String[] args) {
-        try (Scanner scanner = new Scanner(System.in)) { // Create a Scanner object to read input
+        Scanner scanner = new Scanner(System.in);// Create a Scanner object to read input
 
-            System.out.print("Enter name: ");
-            // String name = scanner.next(); // Reads a single word
-            String name = scanner.nextLine(); // Read a line of text (String)
+        System.out.print("Enter name: ");
+        // String name = scanner.next(); // Reads a single word
+        String name = scanner.nextLine(); // Read a line of text (String)
 
-            System.out.print("Enter age: ");
-            int age = scanner.nextInt(); // Read an integer
+        System.out.print("Enter age: ");
+        int age = scanner.nextInt(); // Read an integer
 
-            System.out.print("Enter Income: ");
-            double height = scanner.nextDouble(); // Read a double
+        System.out.print("Enter Income: ");
+        double height = scanner.nextDouble(); // Read a double
 
-            System.out.println("Name is " + name);
-            System.out.println("Age is " + age);
-            System.out.println("Income is " + height);
-        }
+        System.out.println("Name is " + name);
+        System.out.println("Age is " + age);
+        System.out.println("Income is " + height);
     }
 }
